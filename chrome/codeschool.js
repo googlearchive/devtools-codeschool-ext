5f3c1c7fe6f0752e0bcc146b6b7cecc1e59e1186
Nikita Vasilyev <me@elv1s.ru>
2012-11-17 02:44:36 +0400

    Start to work on "check element for forced element state"

--- chrome/codeschool.js
+++ chrome/codeschool.js
@@ -1,1 +1,34 @@
-console.log('YEAH', WebInspector);
+function emitAction(name, data) {
+    console.log({
+        action: name,
+        date: Date.now(),
+        data: data
+    })
+}
+
+
+// check element for forced element state
+(function() {
+
+    var originalMethod = WebInspector.ElementsPanel.prototype._setPseudoClassForNodeId;
+
+    WebInspector.ElementsPanel.prototype._setPseudoClassForNodeId = function(nodeId, pseudoClass, enable) {
+        originalMethod.apply(this, arguments);
+
+        if (!enable) {
+            return;
+        }
+
+        var node = WebInspector.domAgent.nodeForId(nodeId);
+        if (!node) {
+            return;
+        }
+
+        var id = node.getAttribute("id");
+
+        emitAction("forcedElementState", {
+            id: id,
+            state: pseudoClass
+        });
+    }
+})();
