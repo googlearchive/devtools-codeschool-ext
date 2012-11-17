c0de428ed6f93a6612b725d4d78712b764291a37
Nikita Vasilyev <me@elv1s.ru>
2012-11-17 14:12:53 +0400

    Fix: WebInspector.ElementsPanel is undefined during the initial load because it loads lazily via XHR
    It's not ready, it breaks DevTools:
        Uncaught TypeError: undefined is not a function Panel.js:314

--- chrome/codeschool.js
+++ chrome/codeschool.js
@@ -6,29 +6,80 @@ function emitAction(name, data) {
     })
 }
 
+var modulesToFiles = {
+    'WebInspector.ElementsPanel': 'ElementsPanel.js'
+};
+var _callbacks = {};
+
+
+/**
+ * @param {string} path such as 'WebInspector.ElementsPanel.prototype._setPseudoClassForNodeId'
+ * @return {boolean}
+ */
+function isDefined(path) {
+    var obj = window;
+    var keys = path.split('.');
+    var key = '';
+    while (key = keys.shift()) {
+        if (obj.hasOwnProperty(key))
+            obj = obj[key];
+        else
+            return false;
+    }
+    return true;
+}
+
+function onScriptLoad(path, callback) {
+    if (isDefined(path))
+        callback();
+    else {
+        var scriptName = modulesToFiles['WebInspector.ElementsPanel'];
+        if (_importedScripts[scriptName])
+            callback();
+        else {
+            if (!_callbacks[scriptName])
+                _callbacks[scriptName] = [];
+
+            _callbacks[scriptName].push(callback);
+        }
+    }
+}
+
+
+/**
+ * @param {string} scriptName
+ */
+function importScript(scriptName)
+{
+    if (_importedScripts[scriptName])
+        return;
+
+    if (_callbacks[scriptName])
+        _callbacks[scriptName].forEach(function(callback) {
+            callback();
+        })
+}
+
 
 // check element for forced element state
-(function() {
+onScriptLoad('WebInspector.ElementsPanel', function() {
 
     var originalMethod = WebInspector.ElementsPanel.prototype._setPseudoClassForNodeId;
 
     WebInspector.ElementsPanel.prototype._setPseudoClassForNodeId = function(nodeId, pseudoClass, enable) {
         originalMethod.apply(this, arguments);
 
-        if (!enable) {
+        if (!enable)
             return;
-        }
 
         var node = WebInspector.domAgent.nodeForId(nodeId);
-        if (!node) {
+        if (!node)
             return;
-        }
 
         var id = node.getAttribute("id");
-
         emitAction("forcedElementState", {
             id: id,
             state: pseudoClass
         });
     }
-})();
+});
