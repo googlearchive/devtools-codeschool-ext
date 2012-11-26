f1b442c46d63070625d971b5a198a98e01ef7992
Nikita Vasilyev <me@elv1s.ru>
2012-11-27 00:54:00 +0400

    Bring back runtimeEval since we agreed to use a single page (no popups)
    Also, fix premature events initialization

--- chrome/codeschool.js
+++ chrome/codeschool.js
@@ -1,35 +1,81 @@
+function emitAction(data) {
+    console.log(data);
+    data.date = Date.now();
+    var code = 'window.devToolsCallback(' + JSON.stringify(data) + ')';
+    runtimeEval(code);
+}
+
+/**
+ * eval in web page context, e.g. CodeSchool page
+ * @param {string} expression
+ */
+function runtimeEval(expression) {
+    // https://developers.google.com/chrome-developer-tools/docs/protocol/1.0/runtime#command-evaluate
+    RuntimeAgent.evaluate(
+            expression,
+            /*objectGroup*/ '',
+            /*includeCommandLineAPI*/ false,
+            /*doNotPauseOnExceptionsAndMuteConsole*/ false,
+            undefined,
+            /*returnByValue*/ false,
+            /*generatePreview*/ false,
+            function evalCallback(result, meta, wasThrown) {
+                if (wasThrown) {
+                    console.warn(result);
+                }
+            });
+}
+
+
 WebInspector.notifications.addEventListener(WebInspector.UserMetrics.UserAction, function(event) {
     emitAction(event.data);
 });
 
 
-WebInspector.notifications.addEventListener(WebInspector.Events.InspectorLoaded, function() {
+WebInspector.panel('profiles').addEventListener('profile added', function(event) {
+    emitAction({
+        action: 'profileAdded',
+        type: event.data.type
+    });
+});
+
+
+var inspectorLoadedHandlers = [];
+
+function listenTimeline() {
     WebInspector.timelineManager.addEventListener(WebInspector.TimelineManager.EventTypes.TimelineStarted, function() {
         emitAction({
             action: 'timelineSnapshot'
         });
     });
+}
 
+if (WebInspector.timelineManager) {
+    listenTimeline();
+} else {
+    inspectorLoadedHandlers.push(listenTimeline);
+}
+
+
+function listenSettings() {
     WebInspector.settings.pauseOnExceptionStateString.addChangeListener(function(event) {
         emitAction({
             action: 'pauseOnException',
             state: event.data
         });
     });
-
-    WebInspector.panel('profiles').addEventListener('profile added', function(event) {
-        emitAction({
-            action: 'profileAdded',
-            type: event.data.type
-        });
-    });
-});
-
-
-function emitAction(data) {
-    console.log(data);
-    window.postMessage(data, 'chrome-devtools://devtools');
 }
+
+if (WebInspector.settings && WebInspector.settings.pauseOnExceptionStateString) {
+    listenSettings();
+} else {
+    inspectorLoadedHandlers.push(listenTimeline);
+}
+
+
+inspectorLoadedHandlers.forEach(function(handler) {
+    WebInspector.notifications.addEventListener(WebInspector.Events.InspectorLoaded, handler);
+});
 
 
 //@ sourceURL=codeschool.js
--- chrome/index.js
+++ chrome/index.js
@@ -2,14 +2,6 @@ if (location.protocol === 'chrome-devtools:') (function(){
 
     console.log('Code School extension initialized');
 
-    window.addEventListener('message', function(event) {
-        if (event.origin !== 'chrome-devtools://devtools') {
-            return;
-        }
-
-        // TODO: send to background page
-    }, false);
-
     // http://stackoverflow.com/a/4854189/16185
     var script = document.createElement('script');
     script.type = 'text/javascript';
