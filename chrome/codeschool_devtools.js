faedc6f8b6b535a17f206cee4e7ab3e8a6010afe
Nikita Vasilyev <me@elv1s.ru>
2013-03-04 14:42:32 -0500

    Don't console.log objects

--- chrome/codeschool_devtools.js
+++ chrome/codeschool_devtools.js
@@ -1,10 +1,10 @@
 if (location.protocol === 'chrome-devtools:') (function() {
 
-    console.log('Code School extension initialized');
+    console.info('Code School extension initialized');
 
     var port = chrome.extension.connect({name: "devtools"});
     port.onMessage.addListener(function(msg) {
-        console.log('port', msg);
+        console.info("port {command: '%s', url: '%s'}", msg.command, msg.url);
         if (msg.command == 'initialize') {
             if (!msg.url) {
                 throw new Error('url is missing');
@@ -18,7 +18,6 @@ if (location.protocol === 'chrome-devtools:') (function() {
     });
 
     window.addEventListener('message', function(event) {
-        console.log('message', event);
         if (event.data && event.data.command === 'emit') {
             port.postMessage(event.data);
         }
